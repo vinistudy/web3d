5fd35b10fc718e4fc86117d93626aae28e006a1c
zhoulin <zhoulin@xiangrong.pro>
2023-05-28 09:35:46 +0800

    加入天空镜以及地板

--- src/main.js
+++ src/main.js
@@ -19,10 +19,18 @@ const scene = new BABYLON.Scene(engine)
  * @target 相机的目标点
  * @scene 相机所在的场景
  */
-const camera = new BABYLON.ArcRotateCamera('camera', 0, 0, 0, BABYLON.Vector3.Zero(), scene)
+// const camera = new BABYLON.ArcRotateCamera('camera', 0, 0, 0, BABYLON.Vector3.Zero(), scene)
+const camera = new BABYLON.ArcRotateCamera('camera', Math.PI / 2, Math.PI / 4, 10, new BABYLON.Vector3(0, -5, 0), scene)
+
+camera.lowerRadiusLimit = 4;
+camera.upperRadiusLimit = 32;
+camera.wheelDeltaPercentage = 0.01;
 
 // 设置相机的位置
 camera.setPosition(new BABYLON.Vector3(0, 5, 10))
+
+// zoom 速度
+camera.wheelPrecision = 1.8;
 
 // 将相机的目标指向场景的原点
 camera.setTarget(BABYLON.Vector3.Zero());
@@ -30,20 +38,39 @@ camera.setTarget(BABYLON.Vector3.Zero());
 // 把相机附加到画布上, true 阻止默认操作
 camera.attachControl(canvas, true)
 
-// 顶部球形光
-const hemisphericLight1 = new BABYLON.HemisphericLight(
-    'light',  //光源的名称
-    new BABYLON.Vector3(1, 1, 0),  //光源的方向
-    scene //光源所在的场景
-)
+const light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+light.intensity = 0.6;
+light.specular = BABYLON.Color3.Black();
 
-// 底部球形光
-const hemisphericLight2 = new BABYLON.HemisphericLight(
+const light2 = new BABYLON.DirectionalLight("dir01", new BABYLON.Vector3(0, -0.5, -1.0), scene);
+light2.position = new BABYLON.Vector3(0, 5, 5);
+
+const light3 = new BABYLON.HemisphericLight(
     'light',  //光源的名称
-    new BABYLON.Vector3(1, -1, 0),  //光源的方向
+    new BABYLON.Vector3(0, -1, 0),  //光源的方向
     scene //光源所在的场景
 )
-hemisphericLight2.intensity = 0.72
+light3.intensity = 0.36
+
+// // 去掉默认的背景颜色
+// // scene.clearColor = new BABYLON.Color4(1, 0, 0, 0);
+
+const skybox = BABYLON.MeshBuilder.CreateBox("skyBox", { size: 1000.0 }, scene);
+const skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+skyboxMaterial.backFaceCulling = false;
+skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("models/b", scene);
+skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+skybox.material = skyboxMaterial;
+
+const ground = BABYLON.MeshBuilder.CreateGround("ground", { height: 50, width: 50, subdivisions: 4 }, scene);
+const groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
+groundMaterial.diffuseTexture = new BABYLON.Texture("models/wood.jpg", scene);
+groundMaterial.diffuseTexture.uScale = 30;
+groundMaterial.diffuseTexture.vScale = 30;
+groundMaterial.specularColor = new BABYLON.Color3(.1, .1, .1);
+ground.material = groundMaterial;
 
 // 隐藏 loading 画面
 BABYLON.SceneLoader.ShowLoadingScreen = false
